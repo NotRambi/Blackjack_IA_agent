30307696af108a90a2450d337965123ac8a4aadc
Alessio Lavore <61512398+NotRambi@users.noreply.github.com>
2024-09-25 13:42:17 +0200

    Update project
    aggiornato il codice dividendolo in 3 file:
    GUI gestisce il menu di avvio
    Game gestisce l'istanza della partita
    Blackjack fornisce le classi per Game (Giocatore, AgenteIA, Dealer)

---
+++ Blackjack.py
@@ -0,0 +1,318 @@
+# File contenente le classi per la gestione del gioco del BlackJack
+
+class Giocatore:
+    def __init__(self, nome, soldi):
+        self.nome = nome
+        self.soldi = soldi
+        self.mano = [] 
+        self.valoreMano = 0
+        self.bet = 0
+        self.blackjack = False # serie di flag per lo stato del giocatore
+        self.sballa = False
+        self.assicurazione = False
+        self.split = False
+        self.doppio = False
+
+    def toStr(self): # funzione per stampare lo stato del giocatore
+        return 'nome: '+self.nome+', soldi: '+str(self.soldi)
+    
+    def ValoreCarta(self, carta): # funzione per calcolare il valore di una carta
+        if len(carta) == 3:
+            return 10
+        else:
+            if carta[0] == '1':
+                return 11
+            else:
+                return int(carta[0])
+    
+    def addCard(self, carta): # aggiunge una carta alla mano del giocatore e ne calcola il valore
+        self.mano.append(carta)
+        self.valoreMano += self.ValoreCarta(carta)
+        if self.valoreMano > 21:
+            for i in range(0, len(self.mano)):
+                if self.mano[i] == '1c' or self.mano[i] == '1q' or self.mano[i] == '1f' or self.mano[i] == '1p':
+                    self.mano[i] = '1C' if self.mano[i] == '1c' else '1Q' if self.mano[i] == '1q' else '1F' if self.mano[i] == '1f' else '1P'
+                    self.valoreMano -= 10
+                    break
+            if self.valoreMano > 21:
+                self.sballa = True
+        if len(self.mano) == 2 and self.valoreMano == 21:
+            self.blackjack = True
+
+    def toStrMano(self): # funzione per stampare la mano del giocatore
+        return 'mano: '+str(self.mano)+', valore mano: '+str(self.valoreMano)+ (' B' if self.blackjack else '')+ (' S' if self.sballa else '')
+    
+    def assicura(self): # funzione per assicurare la mano
+        self.assicurazione = True
+        self.soldi -= self.bet/2
+
+    def raddoppia(self): # funzione per raddoppiare la puntata
+        self.doppio = True
+        self.soldi -= self.bet
+        self.bet *= 2
+    
+    def resetMano(self): # funzione di reset a fine mano
+        self.mano = []
+        self.valoreMano = 0
+        self.bet = 0
+        self.blackjack = False
+        self.sballa = False
+        self.assicurazione = False
+        self.split = False
+        self.doppio = False
+
+# definizione di agente IA, sotto-classe di giocatore
+class AgenteIA(Giocatore):
+    def __init__(self, nome, soldi, mazzo_intero, num_mazzi):
+        super().__init__(nome, soldi) # eredita tutti i parametri di giocatore
+        self.mazzo_intero = mazzo_intero
+        self.numDiMazzi = num_mazzi
+        self.mazzo = mazzo_intero.copy() * num_mazzi # mazzo_intero e num_mazzi compongono la KB iniziale dell'agente
+        self.carteUscite = []                        # tutti gli altri parametri sono osservazioni della partita 
+        self.manoDealer = []
+        self.BetUnits = 0.01 * self.soldi
+
+    def resetMazzo(self): # funzione per resettare il mazzo quando viene rimescolato raggiunta la taglia
+        self.mazzo = self.mazzo_intero.copy() * self.numDiMazzi
+        self.carteUscite = []
+
+    def updateMazzo(self, uscite):
+        self.resetMazzo()
+        self.carteUscite = uscite.copy()
+        for carta in uscite:
+            self.mazzo.remove(carta)
+    
+    def resetMano(self): # override del reset di giocatore
+        self.mano = []
+        self.valoreMano = 0
+        self.manoDealer = []
+        self.bet = 0
+        self.blackjack = False
+        self.sballa = False
+        self.assicurazione = False
+        self.split = False
+        self.doppio = False
+
+    def addCardToDealer(self, carta): # aggiorna mano e valore del dealer
+        self.manoDealer.append(carta)
+
+    def HandValueCalc(self, hand): # funzione per calcolare il valore di una mano
+        Hand = hand.copy()
+        Val = 0
+        for card in Hand:
+            if len(card) == 3:
+                Val += 10
+            elif card[0] == '1':
+                Val += 11
+            else:
+                Val += int(card[0])
+        
+        while Val > 21:
+            flag = False
+            for card in Hand:
+                if len(card) == 2 and card[0] == '1':
+                    Val -= 10
+                    Hand.remove(card)
+                    flag = True
+            if not flag:
+                break
+        return Val
+    
+    def InferenzaProbabilità(self,i,dictMazzo,lenMazzo,dictProbHand,flagAsso): # funzione ricorsiva per calcolare la distribuzione di probabilità del dealer
+        if i >= 17: # caso base
+            return dictProbHand
+        prob = dictProbHand[i]
+        for e in dictMazzo:
+            if dictMazzo[e] == 0:
+                continue
+            flag = False
+            flag2 = flagAsso
+            if len(e) == 3:
+                if i+10 <= 21:
+                    dictProbHand[i+10] = dictProbHand[i+10] + prob*dictMazzo[e]/lenMazzo
+                    i2 = i+10
+                elif flagAsso:
+                    flag2 = False
+                    dictProbHand[i-10+10] = dictProbHand[i-10+10] + prob*dictMazzo[e]/lenMazzo
+                    i2 = i-10+10
+                else:
+                    dictProbHand[22] = dictProbHand[22] + prob*dictMazzo[e]/lenMazzo
+                    i2 = 22
+            elif e[0] == '1':
+                if i+11 <= 21:
+                    dictProbHand[i+11] = dictProbHand[i+11] + prob*dictMazzo[e]/lenMazzo
+                    i2 = i+11
+                    flag = True
+                elif i+1 <= 21:
+                    dictProbHand[i+1] = dictProbHand[i+1] + prob*dictMazzo[e]/lenMazzo
+                    i2 = i+1
+                else:
+                    dictProbHand[22] = dictProbHand[22] + prob*dictMazzo[e]/lenMazzo
+                    i2 = 22
+            else:
+                if i+int(e[0]) <= 21:
+                    dictProbHand[i+int(e[0])] = dictProbHand[i+int(e[0])] + prob*dictMazzo[e]/lenMazzo
+                    i2 = i+int(e[0])
+                elif flagAsso:
+                    flag2 = False
+                    dictProbHand[i-10+int(e[0])] = dictProbHand[i-10+int(e[0])] + prob*dictMazzo[e]/lenMazzo
+                    i2 = i-10+int(e[0])
+                else:
+                    dictProbHand[22] = dictProbHand[22] + prob*dictMazzo[e]/lenMazzo
+                    i2 = 22
+            dictProbHand[i] = dictProbHand[i] - prob*dictMazzo[e]/lenMazzo
+            newMazzo = dictMazzo.copy()
+            newMazzo[e] -= 1
+            if flag:
+                dictProbHand = self.InferenzaProbabilità(i2,newMazzo,lenMazzo-1,dictProbHand,True)
+            else:
+                dictProbHand = self.InferenzaProbabilità(i2,newMazzo,lenMazzo-1,dictProbHand,flag2)
+        return dictProbHand
+    
+    def ChooseCarta(self, MyHand,DealerHand): # funzione chiamata per decidere se chiedere carta o passare
+
+        # casi base
+        if self.HandValueCalc(MyHand) > 20: # caso in cui si ha già un 21
+            return 0 # passo
+        elif self.HandValueCalc(MyHand) < 12: # caso in cui si ha un 11 o meno (matematicamente impossibile sballare)
+            return 1 # carta
+        
+        # creo dizionario con le carte del mazzo
+        dictMazzo = {'1s':0, '2s':0, '3s':0, '4s':0, '5s':0, '6s':0, '7s':0, '8s':0, '9s':0, '10s':0} # rappresendo il mazzo come la quantità di carte presenti per valore
+        for carta in self.mazzo:
+            if len(carta) == 3: # è un 10 o una figura
+                dictMazzo['10s'] += 1
+            elif carta[0] == '1': # è un asso
+                dictMazzo['1s'] += 1
+            else: # è una carta da 2 a 9
+                dictMazzo[carta[0]+'s'] += 1
+
+        lenMazzo = len(self.mazzo)
+
+        # distribuzione di probabilità della mano del giocatore e del dealer
+        dictValMyHand = {2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,21:0,22:0} # 22 è il caso in cui si sballa
+        dictValDealerHand = {2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,21:0,22:0}
+        dictValMyHand[self.HandValueCalc(MyHand)] = 1
+        dictValDealerHand[self.HandValueCalc(DealerHand)] = 1
+        
+        # caso in cui il dealer ha un asso
+        if self.HandValueCalc(DealerHand) == 11:
+            dictValDealerHand = self.InferenzaProbabilità(self.HandValueCalc(DealerHand),dictMazzo,lenMazzo,dictValDealerHand,True)
+        else:
+            dictValDealerHand = self.InferenzaProbabilità(self.HandValueCalc(DealerHand),dictMazzo,lenMazzo,dictValDealerHand,False)
+
+
+        # confronto tra dealer e mano personale se passo
+        probWinPasso = 0
+        probDrawPasso = 0
+        probLosePasso = 0
+        for m in dictValMyHand:
+            for d in dictValDealerHand:
+                if m == 22:
+                    probLosePasso += dictValMyHand[m]*dictValDealerHand[d]
+                elif d == 22:
+                    probWinPasso += dictValMyHand[m]*dictValDealerHand[d]
+                else:
+                    if m > d:
+                        probWinPasso += dictValMyHand[m]*dictValDealerHand[d]
+                    elif m == d:
+                        probDrawPasso += dictValMyHand[m]*dictValDealerHand[d]
+                    else:
+                        probLosePasso += dictValMyHand[m]*dictValDealerHand[d]
+
+        # caso in cui si chiede carta
+        probWinCarta = 0
+        probDrawCarta = 0
+        probLoseCarta = 0
+
+        for carta in self.mazzo:
+            newmazzo = self.mazzo.copy()
+            newmazzo.remove(carta)
+            myNewHand = MyHand.copy()
+            myNewHand.append(carta)
+            dictValMyHand2 = {2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,21:0,22:0}
+            if self.HandValueCalc(myNewHand) <= 21:
+                dictValMyHand2[self.HandValueCalc(myNewHand)] = 1
+            else:
+                dictValMyHand2[22] = 1
+            if self.HandValueCalc(DealerHand) == 11:
+                dictValDealerHand2 = self.InferenzaProbabilità(self.HandValueCalc(DealerHand),dictMazzo,lenMazzo,dictValDealerHand,True)
+            else:
+                dictValDealerHand2 = self.InferenzaProbabilità(self.HandValueCalc(DealerHand),dictMazzo,lenMazzo,dictValDealerHand,False)
+            for m in dictValMyHand2:
+                for d in dictValDealerHand2:
+                    if m == 22:
+                        probLoseCarta += dictValMyHand2[m]*dictValDealerHand2[d]/lenMazzo
+                    elif d == 22:
+                        probWinCarta += dictValMyHand2[m]*dictValDealerHand2[d]/lenMazzo
+                    else:
+                        if m > d:
+                            probWinCarta += dictValMyHand2[m]*dictValDealerHand2[d]/lenMazzo
+                        elif m == d:
+                            probDrawCarta += dictValMyHand2[m]*dictValDealerHand2[d]/lenMazzo
+                        else:
+                            probLoseCarta += dictValMyHand2[m]*dictValDealerHand2[d]/lenMazzo
+
+        chooseCarta = probWinCarta + probDrawCarta*0.2
+        choosePasso = probWinPasso + probDrawPasso*0.2
+
+        if chooseCarta > choosePasso:
+            return 1 # carta
+        else:
+            return 0 # passo
+        
+    def chooseBet(self):
+        RunningCount = 0
+        for carta in self.carteUscite:
+            if carta[0] == '1':
+                RunningCount -= 1
+            elif carta[0] == '2' or carta[0] == '3' or carta[0] == '4' or carta[0] == '5' or carta[0] == '6':
+                RunningCount += 1
+        numMazziRimanenti = len(self.mazzo)/52
+        TrueCount = int(RunningCount/numMazziRimanenti)
+        if TrueCount < 0:
+            return 2
+        if TrueCount == 0:
+            return self.BetUnits
+        else:
+            return int((TrueCount+1)*self.BetUnits)
+    
+# definizione di dealer
+class dealer:
+    def __init__(self):
+        self.mano = []
+        self.valoreMano = 0
+        self.blackjack = False
+        self.sballa = False
+
+    def ValoreCarta(self, carta): # funzione per calcolare il valore di una carta
+        if len(carta) == 3:
+            return 10
+        else:
+            if carta[0] == '1':
+                return 11
+            else:
+                return int(carta[0])
+    
+    def addCard(self, carta): # aggiunge una carta alla mano del dealer e ne calcola il valore
+        self.mano.append(carta)
+        self.valoreMano += self.ValoreCarta(carta)
+        if self.valoreMano > 21:
+            for i in range(0, len(self.mano)):
+                if self.mano[i] == '1c' or self.mano[i] == '1q' or self.mano[i] == '1f' or self.mano[i] == '1p':
+                    self.mano[i] = '1C' if self.mano[i] == '1c' else '1Q' if self.mano[i] == '1q' else '1F' if self.mano[i] == '1f' else '1P'
+                    self.valoreMano -= 10
+                    break
+            if self.valoreMano > 21:
+                self.sballa = True
+        if len(self.mano) == 2 and self.valoreMano == 21: # condizione per il blackjack
+            self.blackjack = True
+
+    def toStrMano(self):
+        return 'mano: '+str(self.mano)+', valore mano: '+str(self.valoreMano)+ (' B' if self.blackjack else '')+ (' S' if self.sballa else '')
+    
+    def resetMano(self):
+        self.mano = []
+        self.valoreMano = 0
+        self.blackjack = False
+        self.sballa = False
--- GUI.py
+++ GUI.py
@@ -1,15 +1,32 @@
+### LIBRERIE ###
+
 from tkinter import *
 from tkinter import messagebox
 from threading import Thread
+import tempfile
+import Game
 
-class GUI:
+### CLASSI ###
+
+# Classe Principale
+
+class GUI(object):
     def __init__(self):
 
+        # cartella in User/AppData/Local/Temp per file temporanei
+        self.tempfolder = tempfile.TemporaryDirectory() 
+
+        # thread per la partita
+        self.t = None
+
+        # Parametri di gioco
         self.numPlayers = 1
         self.numIA = 0
         self.startingMoney = 1000
         self.stats = False
+        self.animations = True
 
+        # Finestra GUI
         self.window = Tk()
         self.window.title("Blackjack")
         self.window.geometry("400x600")
@@ -20,7 +37,8 @@ class GUI:
 
     def on_closing(self):
         if messagebox.askokcancel("Esci", "Vuoi uscire?"):
-            self.window.destroy()
+            self.tempfolder.cleanup() # cancella la cartella temporanea
+            self.window.destroy() # chiude la finestra
 
     def update(self):
         self.window.update()
@@ -31,12 +49,17 @@ class GUI:
 
     # Home Page
     def HomePage(self):
+        # titolo
         self.HP_title = Label(self.window, text="Blackjack", font=('Arial', 24), bg="#222", fg="#eee")
         self.HP_title.place(x=200, y=100, anchor="center")
+
+        # buttons
         self.Play_Button = Button(self.window, text="Gioca",  font=('Arial', 14), command=self.PlayButton, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
         self.Play_Button.place(x=200, y=300, anchor="center", width=150)
+        
         self.Settings_Button = Button(self.window, text="Impostazioni",  font=('Arial', 14), command=self.SettingsButton, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
         self.Settings_Button.place(x=200, y=375, anchor="center", width=150)
+        
         self.QuitButton = Button(self.window, text="Esci",  font=('Arial', 14), command=self.on_closing, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
         self.QuitButton.place(x=200, y=450, anchor="center", width=150)
 
@@ -56,25 +79,39 @@ class GUI:
 
     # Game Page
     def GamePage(self):
+        # titolo
         self.Game_title = Label(self.window, text="Gioco", font=('Arial', 24), bg="#222", fg="#eee")
         self.Game_title.place(x=200, y=100, anchor="center")
 
+        # buttons
         self.stop_btn = Button(self.window, text="Arresta", font=('Arial', 14), command=self.stopGame, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
         self.stop_btn.place(x=200, y=300, anchor="center", width=150)
 
         self.Back_Button = Button(self.window, text="Indietro", font=('Arial', 14), command=self.Game_backToHP, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
         self.Back_Button.place(x=70, y=560, anchor="center", width=100, height=40)
         
-        self.t = Thread(target=self.startGame())
-        self.t.start()
+        # avvia partita
+        self.startGame()
 
     def startGame(self):
-        pass
-        # avvia il gioco passando a BlackJack i parametri
+        path = self.tempfolder.name + "/temp.txt"
+        self.f = open(path, "w")
+        self.f.write("0")  # questo valore indica che il gioco non deve fermarsi  
+        self.f.close()    
+        if self.t is None:
+            NewGame = Game.Game(self.numPlayers, self.numIA, self.startingMoney, self.stats, self.animations, self.tempfolder.name)
+            self.t = Thread(target=NewGame.RunGame)
+            self.t.start()
+        elif not self.t.is_alive():
+            NewGame = Game.Game(self.numPlayers, self.numIA, self.startingMoney, self.stats, self.animations, self.tempfolder.name)
+            self.t = Thread(target=NewGame.RunGame)
+            self.t.start()
 
     def stopGame(self):
-        pass
-        # chiama la funzione StopGameAtEndOfThisHand() di BlackJack
+        path = self.tempfolder.name + "/temp.txt"
+        self.f = open(path, "w")
+        self.f.write("1") # questo valore indica che il gioco deve fermarsi
+        self.f.close()
 
     def Game_backToHP(self):
         self.clearGame()
@@ -87,9 +124,11 @@ class GUI:
 
     # Settings Page
     def SettingsPage(self):
+        # titolo
         self.Settings_title = Label(self.window, text="Impostazioni", font=("Arial", 24), bg="#222", fg="#eee")
         self.Settings_title.place(x=200, y=100, anchor="center")
 
+        # calcolo bounds per gli slider
         if self.numPlayers + self.numIA > 2:
             if self.numPlayers == 0:
                 lowerBoundP = 0
@@ -117,30 +156,43 @@ class GUI:
             else:
                 lowerBoundP = 0
                 lowerBoundIA = 1
+
+        # sliders
         self.Players_label = Label(self.window, text="Giocatori", font=("Arial", 14), bg="#222", fg="#eee")
-        self.Players_label.place(x=200, y=200, anchor="center")
+        self.Players_label.place(x=200, y=170, anchor="center")
         self.Players_slider = Scale(self.window, from_=lowerBoundP, to=7-self.numIA, orient=HORIZONTAL, length=200, command=self.updateSlider, bg="#222", fg="#eee", highlightbackground="#222", troughcolor="#333", sliderlength=50, sliderrelief="flat", borderwidth=1, relief="flat", activebackground="#111")
         self.Players_slider.set(self.numPlayers)
-        self.Players_slider.place(x=200, y=230, anchor="center")
+        self.Players_slider.place(x=200, y=200, anchor="center")
+
         self.IA_label = Label(self.window, text="IA", font=("Arial", 14), bg="#222", fg="#eee")
-        self.IA_label.place(x=200, y=300, anchor="center")
+        self.IA_label.place(x=200, y=270, anchor="center")
         self.IA_slider = Scale(self.window, from_=lowerBoundIA, to=7-self.numPlayers, orient=HORIZONTAL, length=200, command=self.updateSlider, bg="#222", fg="#eee", highlightbackground="#222", troughcolor="#333", sliderlength=50, sliderrelief="flat", borderwidth=1, relief="flat", activebackground="#111")
         self.IA_slider.set(self.numIA)
-        self.IA_slider.place(x=200, y=330, anchor="center")
+        self.IA_slider.place(x=200, y=300, anchor="center")
 
+        # input
         self.starting_money_label = Label(self.window, text="Soldi iniziali", font=("Arial", 14), bg="#222", fg="#eee")
-        self.starting_money_label.place(x=150, y=420, anchor="center")
-        self.starting_money_entry = Entry(self.window, font=("Arial", 14), bg="#333", fg="#eee", relief="flat", borderwidth=0, highlightbackground="#222", width=5, insertbackground="#eee")
+        self.starting_money_label.place(x=150, y=380, anchor="center")
+        self.starting_money_entry = Entry(self.window, font=("Arial", 14), bg="#333", fg="#eee", relief="flat", borderwidth=0, highlightbackground="#222", insertbackground="#eee")
         self.starting_money_entry.insert(0, self.startingMoney)
-        self.starting_money_entry.place(x=250, y=420, anchor="center", width=50)
+        self.starting_money_entry.place(x=250, y=380, anchor="center", width=60, height=35)
 
+        # buttons
         self.stats_label = Label(self.window, text="Statistiche", font=("Arial", 14), bg="#222", fg="#eee")
-        self.stats_label.place(x=150, y=460, anchor="center")
+        self.stats_label.place(x=150, y=430, anchor="center")
         if self.stats:
             self.statistics_btn  = Button(self.window, text="ON", font=("Arial", 14), command=self.statisticsBtn, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
         else:
             self.statistics_btn  = Button(self.window, text="OFF", font=("Arial", 14), command=self.statisticsBtn, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
-        self.statistics_btn.place(x=250, y=460, anchor="center", width=50)
+        self.statistics_btn.place(x=250, y=430, anchor="center", width=60, height=35)
+
+        self.anim_label = Label(self.window, text="Animazioni", font=("Arial", 14), bg="#222", fg="#eee")
+        self.anim_label.place(x=150, y=480, anchor="center")
+        if self.animations:
+            self.animations_btn  = Button(self.window, text="ON", font=("Arial", 14), command=self.animationsBtn, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
+        else:
+            self.animations_btn  = Button(self.window, text="OFF", font=("Arial", 14), command=self.animationsBtn, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
+        self.animations_btn.place(x=250, y=480, anchor="center", width=60, height=35)
 
         self.Back_Button = Button(self.window, text="Indietro", font=("Arial", 14), command=self.Setting_backToHP, bg="#333", fg="#eee", activebackground="#111", activeforeground="#eee", borderwidth=0)
         self.Back_Button.place(x=70, y=560, anchor="center", width=100, height=40)
@@ -185,6 +237,13 @@ class GUI:
         else:
             self.statistics_btn.config(text="OFF")
 
+    def animationsBtn(self):
+        self.animations = not self.animations
+        if self.animations:
+            self.animations_btn.config(text="ON")
+        else:
+            self.animations_btn.config(text="OFF")
+
     def Setting_backToHP(self):
         self.startingMoney = int(self.starting_money_entry.get())
         self.clearSettings()
@@ -200,8 +259,12 @@ class GUI:
         self.starting_money_entry.destroy()
         self.stats_label.destroy()
         self.statistics_btn.destroy()
+        self.anim_label.destroy()
+        self.animations_btn.destroy()
         self.Back_Button.destroy()
 
+
+### MAIN ###
 
 if __name__ == "__main__":
     RunGUI = GUI()
